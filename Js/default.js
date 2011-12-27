89ac8b192d91e9ec5a40a8d474bb5020ddd3b736
haasin khan <haaisnonly@gmail.com>
2011-12-27 09:37:57 +0400

    mycommit

--- Js/default.js
+++ Js/default.js
@@ -1,37 +1,30 @@
 function executeHandler(){
- $(document).ready(function(){    
-   justForTest(); 
-});
- 
-  function justForTest() {
-  
-    var url = window.location.href;  
+    $(document).ready(function(){
+        justForTest();
+    });
     
-	$('input[type=button]').click(function(){	  	  
-	  $('input[type=text]').val(url);
-	});
-   
-    var $newdiv = $("<div class='aJsdiv'><p>Lorem ipsum dolor sit amet</p><p>Lorem ipsum dolor sit amet</p></div>").hide();
-	var $close = $("<span class='close'>X</span>");
-    $newdiv.appendTo('div.contact');
-    $close.appendTo($newdiv);
-	
-    $('a.generate').click(function(){
-     $newdiv.fadeIn('slow');
-	 return false;
-	});
+    function justForTest(){
     
-	$close.click(function(){
-	 $(this).parent().fadeOut();
-	});
-	
-  }
-  
+        var url = window.location.href;
+        
+        $('input[type=button]').click(function(){
+            $('input[type=text]').val(url);
+        });
+        
+        var $newdiv = $("<div class='aJsdiv'><p>Lorem ipsum dolor sit amet</p><p>Lorem ipsum dolor sit amet</p></div>").hide();
+        var $close = $("<span class='close'>X</span>");
+        $newdiv.appendTo('div.contact');
+        $close.appendTo($newdiv);
+        
+        $('a.generate').click(function(){
+            $newdiv.fadeIn('slow');
+            return false;
+        });
+        
+        $close.click(function(){
+            $(this).parent().fadeOut();
+        });
+        
+    }
+    
 }
-	
-	
-	
-	
-	
-	
-
